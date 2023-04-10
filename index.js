78bc5c79a9d01d506a621872474d3d6bb6d254d4
Altenia Gaines <gaines.tina90@gmail.com>
2023-04-09 18:24:47 -0700

    use find method

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(record){
+    const results = record.find(record => record.result === "W")
+    if (results){
+        return results.year;
+    }
+}
